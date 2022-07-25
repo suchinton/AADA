65e55cd0be144ebd336c682ebcfcef675656aaa8
suchinton <75079303+suchinton@users.noreply.github.com>
2022-07-25 21:51:05 +0530

    added time keeper

--- Binary_search/binary_search.cpp
+++ Binary_search/binary_search.cpp
@@ -1,59 +1,59 @@
 #include<iostream>
 #include <unistd.h>
+#include <chrono>
 
+using namespace std::chrono;
 using namespace std;
 
 int binarySearch(int A[],int l,int r,int search);
 
- int main()
-   {
-      system("clear");
-      printf("\n| Suchinton (A2345920063)            |");
-      printf("\n|------------------------------------|");
+int main()
+  {
+     system("clear");
+     printf("\n| Suchinton (A2345920063)            |");
+     printf("\n|------------------------------------|");
+     printf("\n|Binary_search-[enter sorted elements|");
+     printf("\n|------------------------------------|");
 
-      printf("\n|Binary_search-[enter sorted elements|");
-      printf("\n|------------------------------------|");
+     int len;
+     int search,result;
 
-      int len, search,result;
-      printf("\nEnter no. of elements you wish to enter: ");
-      scanf("%d",&len);
+     printf("\nEnter range (0-?): 0-");
+     scanf("%d",&len);
 
-      int A[len];
+     int A[len];
 
-      for(int i=0;i<len;i++)
-         {
-            printf("Element %d is: ",i+1);
-            scanf("%d",&A[i]);
-         }
+     for(int i=0;i<=len;i++)
+        {
+           A[i] = i;
+        }
 
-      //system("clear");
+     cout<<"\n\nRange is from: "<<A[0]<<"-"<<A[len];
+     printf("\n\nenter the element to search: ");
+     scanf("%d",&search);
 
-      printf("\n\nEntered array is: \n");
-      for(int i=0;i<len;i++)
-         printf("%d ",A[i]);
+     auto start = high_resolution_clock::now();
+     result = binarySearch(A, 0, len - 1, search);
+     auto stop = high_resolution_clock::now();
 
-      printf("\n\nenter the element to search: ");
-      scanf("%d",&search);
+     if(A[result] != search)
+        printf("\nElement is not present in array\n");
+     else if(A[result]==search && A[result+1]==search)
+        {
+           printf("\nElement is present at position %d",result+1);
+           printf("\nElement is present at position %d",result+2);
+        }
+     else if(A[result]==search && A[result-1]==search)
+        {
+           printf("\nElement is present at position %d",result);
+           printf("\nElement is present at position %d",result+1);
+        }
+     else if(A[result]==search)
+        printf("\nElement is present at position %d\n",result+1);
 
-      result = binarySearch(A, 0, len - 1, search);
-      if(A[result] != search)
-         printf("\nElement is not present in array\n");
-
-      else if(A[result]==search && A[result+1]==search)
-         {
-            printf("\nElement is present at position %d",result+1);
-            printf("\nElement is present at position %d",result+2);
-         }
-
-      else if(A[result]==search && A[result-1]==search)
-         {
-            printf("\nElement is present at position %d",result);
-            printf("\nElement is present at position %d",result+1);
-         }
-
-      else if(A[result]==search)
-         printf("\nElement is present at position %d\n",result+1);
-   }
+     auto duration = duration_cast<microseconds>(stop - start);
+     cout<<"Time taken: "<<duration.count()<<"ms"<<endl;
+  }
 
 int binarySearch(int A[],int l,int r,int search)
    {
