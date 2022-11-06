ebb5a89a3000190b4e2008b49a62fd2c61dd0607
suchinton <75079303+suchinton@users.noreply.github.com>
2022-11-06 13:44:34 +0530

    update

--- 10)_Knapsack_(Backtracking)/prog.cpp
+++ 10)_Knapsack_(Backtracking)/prog.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-int weights[] = {2, 3, 1}, values[] = {6, 15, 7};
+int weights[] = {2, 3, 4, 5}, values[] = {3, 4, 5, 6};
 
-int solution = 0, n = 3;
+int solution = 0, n = 4;
 
 std::vector<int> vsol;
 std::vector<int> temp;
@@ -60,14 +60,25 @@ int main()
 {
     system ("clear");
 
-    printf("\n| Suchinton (A2345920063)    |");
-    printf("\n|----------------------------|");
-    printf("\n| WAP to implement DFS Algo. |");
-    printf("\n|----------------------------|\n");
+    printf("\n| Suchinton (A2345920063)               |");
+    printf("\n|---------------------------------------|");
+    printf("\n| 0-1 Knapsack Prob. using Backtracking |");
+    printf("\n|---------------------------------------|\n");
     
-    Knapsack(0, 2, 0);
-    cout << "solution: " << solution << endl;
+    int KV = 21; //Knapsack value
+
+    cout<<"\n| Ino. | W | V |\n";
+    for(int i=0; i<n; i++)
+      cout<<"|  "<<i+1<<"   | "<<weights[i]<<" | "<<values[i]<<" |\n";
+
+    cout<<"\nn: "<<n;
+    cout<<"\nKnapsack value: "<<KV;
+
+
+    Knapsack(0, KV, 0);
+    cout << "\nsolution: " << solution;
+    cout << "\nItems:  ";
     for(vector<int>::iterator it = vsol.begin(); it != vsol.end(); it++)
-        cout << *it << " ";
+        cout << *it + 1<< " ";
     return 0;
 }
